954a08fd2b05246f7c7594709baaa94bb5435322
student <student@iMac-student.local>
2020-05-09 00:01:24 +0300

    1

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: ametapod <pe4enko111@rambler.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 22:17:57 by ametapod          #+#    #+#             */
-/*   Updated: 2020/05/08 22:17:57 by ametapod         ###   ########.fr       */
+/*   Updated: 2020/05/08 23:54:20 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,16 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char			*str;
 	unsigned int	i;
 
+	if (!s)
+		return (0);
+	i = 0;
 	str = ft_strdup(s);
+	if (!str)
+		return (0);
 	while (str[i])
+	{
 		str[i] = f(i, str[i]);
+		i++;
+	}
 	return (str);
 }

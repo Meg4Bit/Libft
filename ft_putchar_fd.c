c628c14a60218b8004a9825fd93ba73df4ad60e0
student <student@iMac-student.local>
2020-05-09 16:16:10 +0300

    11

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: ametapod <pe4enko111@rambler.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 10:58:16 by ametapod          #+#    #+#             */
-/*   Updated: 2020/05/09 12:50:22 by student          ###   ########.fr       */
+/*   Updated: 2020/05/09 16:13:34 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-	char *buf[2];
+	char buf[3];
 
-	if (c > 127)
+	if ((unsigned char)c > 127)
 	{
-		buff[0] = ((unsigned char)c & 31) | 192;
-        buff[1] = ((unsigned char)c & 63) | 128;
+		buf[0] = (((unsigned char)c >> 6) & 31) | 192;
+        buf[1] = ((unsigned char)c & 63) | 128;
+		buf[2] = 0;
 		write(fd, buf, 2);
 	}
 	else
